37722397c3866acfdf5f497a229951b671d844d2
KonH <konhit@gmail.com>
2024-12-13 19:10:28 +0100

    Trying to setup test coverage

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -3,8 +3,6 @@
 #include "Systems/Movement/PlayerMovementSystemTest.h"
 #include "Systems/Selection/MouseArmySelectionSystemTest.h"
 
-// TODO: integrate code coverage
-
 int main(int argc, char **argv) {
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
